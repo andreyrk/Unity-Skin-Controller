436802bdb1f0f779b0434c9d7b6660249f220da8
Andrey RK <andreyerickk@gmail.com>
2018-01-24 19:21:51 -0200

    Initial commit.

---
+++ Assets/Skin Controller/Scripts/Editor/SkinControllerEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(SkinController))]
+public class SkinControllerEditor : Editor {
+
+	SerializedProperty path;
+	SerializedProperty spriteSheet;
+	SerializedProperty type;
+	SerializedProperty enableWarnings;
+
+	/// <summary>
+	/// Sets up all needed SerialiozedProperty for multiple object editing
+	/// </summary>
+	void OnEnable() {
+		path = serializedObject.FindProperty ("path");
+		spriteSheet = serializedObject.FindProperty ("spriteSheet");
+		type = serializedObject.FindProperty ("type");
+		enableWarnings = serializedObject.FindProperty ("enableWarnings");
+	}
+
+	/// <summary>
+	/// Displays controls and gets inputs on the Inspector window
+	/// </summary>
+	override public void OnInspectorGUI()
+	{
+		serializedObject.Update ();
+
+		var script = target as SkinController;
+
+		path.stringValue = EditorGUILayout.TextField (new GUIContent ("Path to Folder", "(Must be in a Resources folder) The path to the folder that contains the sprite sheet."), script.path);
+		spriteSheet.stringValue = EditorGUILayout.TextField (new GUIContent ("Sprite Sheet Name", "(Must be in a Resources folder) The name of the sprite sheet."), script.spriteSheet);
+
+		EditorGUILayout.PropertyField (type, new GUIContent ("Swap Type", "'Tag' swaps based on the same sprite name in the sheet [shirt] > [shirt]. 'Number' swaps based on its numeric termination [grass_1] > [snow_1]."));
+		enableWarnings.boolValue = EditorGUILayout.Toggle (new GUIContent ("Enable Warnings", "If true warnings will be thrown when a sprite isn't found with the current settings."), script.enableWarnings);
+
+		serializedObject.ApplyModifiedProperties ();
+	}
+}
---
+++ Assets/Skin Controller/Scripts/SkinController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SkinController : MonoBehaviour {
+	
+	SpriteRenderer spriteRenderer;
+
+	public string path;
+	public string spriteSheet;
+	public SpriteNaming type;
+	public bool enableWarnings;
+
+	/// <summary>
+	/// This enum controls which searching method will be used to swap between sprites
+	/// </summary>
+	public enum SpriteNaming {
+		Tag,
+		Number
+	}
+
+	/// <summary>
+	/// Caches the renderer for runtime use.
+	/// </summary>
+	void Start() {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void LateUpdate() {
+		SkinUpdate (spriteRenderer);
+	}
+
+	/// <summary>
+	/// Shows changes in the Scene View
+	/// </summary>
+	void OnDrawGizmos() {
+		SkinUpdate (GetComponent<SpriteRenderer> ());
+	}
+
+	public void SkinUpdate(SpriteRenderer spriteRenderer) {
+		if (spriteSheet != "" && spriteRenderer.sprite) {
+			string spriteName = spriteRenderer.sprite.name;
+
+			Sprite[] otherSprites = Resources.LoadAll<Sprite> ((path != "" ? path + "/" : "") + spriteSheet);
+			Sprite newSprite;
+
+			Predicate<Sprite> match = null;
+
+			if (type == SpriteNaming.Number) {
+				// Match set to get sprites based on its numeric termination
+				match = item => item.name.Substring (item.name.LastIndexOf ('_')) == spriteName.Substring (spriteName.LastIndexOf ('_'));
+			} else if (type == SpriteNaming.Tag) {
+				// Match set to get sprites based on its tag
+				match = item => item.name == spriteName;
+			}
+
+			newSprite = Array.Find (otherSprites, match);
+
+			// Apply changes
+			if (newSprite) {
+				spriteRenderer.sprite = newSprite;
+			} else if (enableWarnings) {
+				Debug.Log ("Swapping issue. Sprite is null.");
+			}
+		}
+	}
+}
